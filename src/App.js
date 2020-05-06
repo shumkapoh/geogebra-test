300084f15197ee9d9b5bcc30540bf963190b5831
Froyo Shum <shumkapoh@hotmail.my>
2020-05-06 15:03:45 +0800

    Geoboar now shows through component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import Board from './component/Board';
 import './App.css';
 
 function App() {
   const board1Ref = useRef(null);
   const testRef = useRef(null);
-  let app;
 
-  const [appId, setAppId] = useState(null);
-
-  useEffect(() => {
-    window.addEventListener('load', () => {
-      app = board1Ref.current.children[0].children[1];
-      setAppId(app.id);
-      console.log(app);
-      console.log(appId);
-    });
-
-    return window.removeEventListener('load', () => {});
-  });
   return (
     <div>
       <Board refs={board1Ref} />
--- src/component/Board.tsx
+++ src/component/Board.tsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Board = ({ refs }: { refs: any }) => {
-  return (<div id="ggb-element" ref={refs} />)
+  useEffect(() => {
+    const ggbApp = new window.GGBApplet(
+      {
+        id: 'app1',
+        appName: 'geometry',
+        width: 800,
+        height: 600,
+        showToolBar: true,
+        showAlgebraInput: true,
+        showMenuBar: true,
+        appletOnLoad: () => {
+          console.log('done loading');
+        },
+      },
+      true
+    );
+
+    window.addEventListener('load', function () {
+      ggbApp.setHTML5Codebase('GeoGebra/HTML5/5.0/web3d/');
+      ggbApp.inject('ggb-element');
+    });
+
+    return () => { };
+  }, []);
+
+  return <div id="ggb-element" ref={refs} />;
+};
+
+declare global {
+  interface Window {
+    GGBApplet: any
+  }
 }
+
 export default Board;
