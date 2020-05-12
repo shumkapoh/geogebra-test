405153e06a23298dfcc9f0a8c16af15bc6cfb161
Froyo Shum <shumkapoh@hotmail.my>
2020-05-12 12:28:42 +0800

    Able to draw

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Board from './component/Board';
 import './App.css';
 
 function App() {
   const board1Ref = useRef(null);
   const testRef = useRef(null);
+  const [x, setX] = useState();
 
   return (
     <div>
-      <Board refs={board1Ref} />
+      {/* <Board refs={board1Ref} id="app1" />
       <input
         value="SetCoords"
-        onClick={() => window.app1.setCoords('A', 5, 1)}
+        onClick={() => window['app1'].setCoords('A', 5, 1)}
         type="button"
       />
-      <div id="test" ref={testRef} />
+      <input
+        value="SetCoords"
+        onClick={() => {
+          const xCoord = window['app1'].getXcoord('A');
+          console.log(xCoord);
+        }}
+        type="button"
+      /> */}
+      <Board id="app1" width={500} height={500} />
     </div>
   );
 }
--- src/component/Board.tsx
+++ src/component/Board.tsx
@@ -1,38 +1,100 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
-const Board = ({ refs }: { refs: any }) => {
-  useEffect(() => {
-    const ggbApp = new window.GGBApplet(
-      {
-        id: 'app1',
-        appName: 'geometry',
-        width: 800,
-        height: 600,
-        showToolBar: true,
-        showAlgebraInput: true,
-        showMenuBar: true,
-        appletOnLoad: () => {
-          console.log('done loading');
-        },
-      },
-      true
-    );
+// GeoBoard
+// const Board = (props: BoardProps) => {
+//   const { refs, id } = props;
+//   useEffect(() => {
+//     const ggbApp = new window.GGBApplet(
+//       {
+//         id: id,
+//         appName: 'geometry',
+//         width: 800,
+//         height: 600,
+//         showToolBar: true,
+//         showAlgebraInput: true,
+//         showMenuBar: true,
+//         appletOnLoad: () => {
+//           console.log('done loading');
+//         },
+//       },
+//       true
+//     );
 
-    window.addEventListener('load', function () {
-      ggbApp.setHTML5Codebase('GeoGebra/HTML5/5.0/web3d/');
-      ggbApp.inject('ggb-element');
-    });
+//     window.addEventListener('load', function () {
+//       ggbApp.setHTML5Codebase('GeoGebra/HTML5/5.0/web3d/');
+//       ggbApp.inject(id);
+//     });
 
-    return () => { };
-  }, []);
+//     return () => { };
+//   }, []);
 
-  return <div id="ggb-element" ref={refs} />;
-};
+//   return <div id={id} ref={refs} />;
+// };
 
-declare global {
-  interface Window {
-    GGBApplet: any
+// declare global {
+//   interface Window {
+//     GGBApplet: any
+//   }
+// }
+
+const Board = (props: BoardProps) => {
+  const { ref, id, width, height } = props;
+  const [canDraw, setCanDraw] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const ctx: CanvasRenderingContext2D | null = canvasRef.current ? canvasRef.current.getContext('2d') : null;
+  const offsetTop = canvasRef.current ? canvasRef.current.offsetTop : 0;
+  const offsetLeft = canvasRef.current ? canvasRef.current.offsetLeft : 0;
+
+  const startPosition = (event: any) => {
+    setCanDraw(true);
+    console.log('startPosition', `x ${event.clientX - offsetLeft}`, `y ${event.clientY - offsetTop}`);
   }
+
+  const endPosition = (event: any) => {
+    setCanDraw(false);
+    if (ctx) {
+      ctx.beginPath();
+    }
+    console.log('endPosition', `x ${event.clientX - offsetLeft}`, `y ${event.clientY - offsetTop}`);
+  }
+
+  const draw = (event: any) => {
+    if (!canDraw) return;
+    console.log(`x ${event.clientX}`, `y ${event.clientY}`)
+
+    if (ctx) {
+      ctx.lineWidth = 10;
+      ctx.lineCap = 'round';
+      ctx.lineTo(event.clientX - offsetLeft, event.clientY - offsetTop);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(event.clientX - offsetLeft, event.clientY - offsetTop);
+    }
+  }
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{ border: '1px solid black', marginLeft: '10%', marginTop: '10%' }}
+      id={id}
+      width={width}
+      height={height}
+      onMouseDown={(e) => startPosition(e)}
+      onMouseUp={(e) => endPosition(e)}
+      onMouseMove={(e) => draw(e)} />
+  )
+}
+
+interface BoardProps {
+  ref: any,
+  id: string,
+  width: number,
+  height: number,
+}
+
+Board.defaultProps = {
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
 export default Board;
